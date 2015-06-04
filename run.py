6a42d8039cf748f622bf03e34300dc9f22f283be
Eric Holscher <eric@ericholscher.com>
2015-06-03 17:14:58 -0700

    Add runner

---
+++ run.py
@@ -0,0 +1,13 @@
+import os
+import datetime
+import time
+
+while 1:
+    with open('time.rst', 'w') as time_file:
+        time_file.write('Time\n====')
+        time_file.write('%s' % datetime.datetime.now())
+    print datetime.datetime.now()
+    os.system('git commit -am Time && git push origin master')
+    time.sleep(60)
+
+
