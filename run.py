febf9b057ea281d4d61f2447a0bfc98ee87f34c6
Eric Holscher <eric@ericholscher.com>
2015-06-03 17:15:55 -0700

    Time

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import time
 
 while 1:
     with open('time.rst', 'w') as time_file:
-        time_file.write('Time\n====')
+        time_file.write('Time\n====\n\n')
         time_file.write('%s' % datetime.datetime.now())
     print datetime.datetime.now()
     os.system('git commit -am Time && git push origin master')
