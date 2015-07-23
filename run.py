3f521992e2e397704512752b993fc3f06119ebc5
Eric Holscher <eric@ericholscher.com>
2015-07-23 14:54:36 -0700

    Time

--- run.py
+++ run.py
@@ -4,12 +4,19 @@ import os
 import datetime
 import time
 
+import requests
+from dateutil.parser import parse
+
 while 1:
     with open('time.rst', 'w') as time_file:
         time_file.write('Time\n====\n\n')
         time_file.write('%s' % datetime.datetime.now())
-    print datetime.datetime.now()
     os.system('git commit -am Time && git push origin master')
+    resp = requests.get('http://readthedocs.org/api/v1/build/?project__slug=time-test&format=json&limit=2&type=html')
+
+    five_minutes_ago = datetime.datetime.now() - datetime.timedelta(minutes=5)
+    for obj in resp.json()['objects']:
+        print "Test: %s" % obj['status'] == True
+        print "Test: %s" % parse(obj['date']) > five_minutes_ago
+
     time.sleep(60)
-
-
