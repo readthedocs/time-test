8040f9bbea37a4855e00dde31eceb43d854b98d1
Eric Holscher <eric@ericholscher.com>
2018-09-28 18:34:28 +0200

    Time

--- org-rtd.py
+++ org-rtd.py
@@ -16,10 +16,10 @@ while 1:
         time_file.write('%s' % datetime.datetime.now())
     os.system('git commit -am Time && git push origin master')
     URL = '{host}/api/v2/build/?project__slug={slug}&format=json&limit=1'.format(host=HOST, slug=SLUG)
-    print URL
+    print(URL)
     resp = requests.get(URL)
     five_minutes_ago = datetime.datetime.now() - datetime.timedelta(minutes=5)
     obj = resp.json()['results'][0]
-    print "Test: %s" % str(obj['success'] == True)
-    print "Test: %s" % str(parse(obj['date']) > five_minutes_ago)
+    print("Test: %s" % str(obj['success'] == True))
+    print("Test: %s" % str(parse(obj['date']) > five_minutes_ago))
     time.sleep(60)
