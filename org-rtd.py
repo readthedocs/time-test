78f115086ae7f1f5818bee78ee66f2b68a2fc6cc
Eric Holscher <eric@ericholscher.com>
2019-07-23 15:09:42 -0700

    Time

--- org-rtd.py
+++ org-rtd.py
@@ -15,7 +15,7 @@ while 1:
     with open('time.rst', 'w') as time_file:
         time_file.write('Time\n====\n\n')
         time_file.write('%s' % datetime.datetime.now())
-    os.system('git commit -am Time && git push origin master')
+    os.system('git commit -am Time && git push')
     URL = '{host}/api/v2/build/?project__slug={slug}&format=json&limit=1'.format(host=HOST, slug=SLUG)
     print(URL)
     resp = requests.get(URL)
